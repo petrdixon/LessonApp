0e993e97aa9f3dcf5eb0660fe41b0a7a327ee7e8
petrdixon <crossout20001@mail.ru>
2022-05-09 10:53:59 +0300

    Push project to gitHub

---
+++ app/src/main/java/com/example/lessonapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.lessonapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Class as Class1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataClass = DataClassExample(exampleInt = 1)
+        val exampleString = dataClass.exampleString
+        val exampleInt = dataClass.exampleInt
+
+
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "$exampleString $exampleInt", Toast.LENGTH_SHORT)
+                .show()
+            button.text = "чего нибудь $exampleString$exampleInt"
+        }
+
+        val copyDataClass = DataClassExample().copy("testString2")
+        button.text = "${copyDataClass.exampleString}"
+
+    }
+}
+
+data class DataClassExample(val exampleString: String = "testString", val exampleInt: Int = 0)
+
+
+
+
